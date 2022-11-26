c60d4a202e062908ee567d4f29f46e456e237d08
Marcos Lins <vinicius.gzm0@gmail.com>
2022-11-26 17:56:14 -0300

    Aula "IF aninhado" completa

---
+++ Plataforma .NET com C#/03 - Tipos de Operadores/05 - If aninhado/Program.cs
@@ -0,0 +1,17 @@
+﻿int quantidadeEstoque = 3;
+int quantidadeCompra = 0;
+bool possivelVenda = quantidadeEstoque >= quantidadeCompra;
+
+
+if (quantidadeCompra == 0)
+{
+    Console.WriteLine("Insira um valor caso queira comprar um produto!");
+}
+else if (possivelVenda)
+{
+    Console.WriteLine("Venda realizada.");
+}
+else
+{
+    Console.WriteLine("Desculpe, não temos a quantia desejada em estoque :(");
+}
