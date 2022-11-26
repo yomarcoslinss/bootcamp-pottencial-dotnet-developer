174a0b8f5ac70c5f00bcd89ff8d9d4b9d2c7b3c4
Marcos Lins <vinicius.gzm0@gmail.com>
2022-11-25 21:09:37 -0300

    Aula "Diferença entre Convert e Parse " completa

---
+++ Plataforma .NET com C#/03 - Tipos de Operadores/01 - Operadores de Atribuição/Program.cs
@@ -0,0 +1,16 @@
+﻿// = é o operador de atribuição
+int a = 10;
+int b = 9;
+int c = a + b; // O valor de C será 19 (a + b)
+Console.WriteLine(c);
+
+// Combinando operadores
+c = c + 5; // c está recebendo o valor c (19) + 5
+Console.WriteLine(c); // Resultado = 24
+
+// O código abaixo trás o mesmo resultado do código acima !
+c += 6; // += significa que a variável c irá receber o seu valor (24) e irá receber uma soma (6)
+Console.WriteLine(c); // Resultado = 30
+
+c -= 15; // O valor atribuido a variável c, é =  c(30) - 15;
+Console.WriteLine(c); // Resultado = 15
---
+++ Plataforma .NET com C#/03 - Tipos de Operadores/02 - Convertendo tipos de variaveis/Program.cs
@@ -0,0 +1,13 @@
+﻿// Cast ou Casting, se refere a conversão de tipos de variáveis
+
+// Usando a classe Convert para converter um número string para um numero inteiro:
+int a = Convert.ToInt32("5");
+Console.WriteLine(a);
+
+//Outra maneira de resolução
+int b = int.Parse("5");
+Console.WriteLine(b);
+
+// A diferença entre as duas soluções acima, é como elas tratam um valor null
+// Caso a classe Convert recebá um valor null, ela irá retornar 0 (recomendado)
+// E no caso do Parse, ele não aceita o null e retorna um erro
