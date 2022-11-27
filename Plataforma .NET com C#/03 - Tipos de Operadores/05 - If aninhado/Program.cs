b9e0fd9615711a21b3b8a1508e60bb733a9269a7
Marcos Lins <vinicius.gzm0@gmail.com>
2022-11-27 15:49:35 -0300

    Aula "Aprendendo o switch case" completa

--- Plataforma .NET com C#/03 - Tipos de Operadores/05 - If aninhado/Program.cs
+++ Plataforma .NET com C#/03 - Tipos de Operadores/05 - If aninhado/Program.cs
@@ -1,11 +1,16 @@
-﻿int quantidadeEstoque = 3;
-int quantidadeCompra = 0;
-bool possivelVenda = quantidadeEstoque >= quantidadeCompra;
+﻿int quantidadeCompra = 1;
 
+
+int quantidadeEstoque = 3;
+bool possivelVenda = quantidadeCompra > 0 && quantidadeEstoque >= quantidadeCompra;
+
+Console.WriteLine($"Quantidade em estoque: {quantidadeEstoque}");
+Console.WriteLine($"Quantidade de compra: {quantidadeCompra}");
+Console.WriteLine($"É possível realizar essa venda? {possivelVenda}");
 
 if (quantidadeCompra == 0)
 {
-    Console.WriteLine("Insira um valor caso queira comprar um produto!");
+    Console.WriteLine("Valor inválido !! digite um valor válido");
 }
 else if (possivelVenda)
 {
---
+++ Plataforma .NET com C#/03 - Tipos de Operadores/06 - Switch case/Program.cs
@@ -0,0 +1,35 @@
+﻿// Console.WriteLine("Digite uma letra");
+// string? letra = Console.ReadLine();
+
+// if (letra == "a" ||
+//     letra == "e" ||
+//     letra == "i" ||
+//     letra == "o" ||
+//     letra == "u")
+// {
+//     Console.WriteLine("A letra digitada é uma VOGAL");
+// }
+// else
+// {
+//     Console.WriteLine("A letra digitada é uma CONSOANTE");
+// }
+
+
+
+Console.WriteLine("Digite uma letra");
+string? letra = Console.ReadLine();
+
+switch (letra)
+{
+    case "a":
+    case "e":
+    case "i":
+    case "o":
+    case "u":
+        Console.WriteLine("A letra digitada é uma VOGAL");
+        break;
+
+    default:
+        Console.WriteLine("A letra digitada é uma CONSOANTE");
+        break;
+}
