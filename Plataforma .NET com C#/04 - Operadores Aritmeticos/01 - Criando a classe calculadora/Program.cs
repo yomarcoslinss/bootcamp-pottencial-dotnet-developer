31c66247f4da1f2e660743977f752de913351594
Marcos Lins <vinicius.gzm0@gmail.com>
2022-11-27 18:46:23 -0300

    Curso "Operadores Aritméticos em C#" completo

---
+++ Plataforma .NET com C#/04 - Operadores Aritmeticos/01 - Criando a classe calculadora/Program.cs
@@ -0,0 +1,14 @@
+﻿using _01___Criando_a_classe_calculadora.models.Calculadora;
+
+Calculadora calc = new Calculadora();
+
+calc.Somar(10, 30);
+calc.Subtrair(10, 50);
+calc.Multiplicar(15, 45);
+calc.Dividir(2, 2);
+calc.Potencia(3, 3);
+// Funções Trigonométricas
+calc.Seno(30);
+calc.Coseno(30);
+calc.Tangente(30);
+calc.RaizQuadrada(25);
---
+++ Plataforma .NET com C#/04 - Operadores Aritmeticos/02 - Incremento e Decremento/Program.cs
@@ -0,0 +1,16 @@
+﻿int numeroIncremento = 10;
+Console.WriteLine(numeroIncremento);
+Console.WriteLine("Incrementando o 10");
+// numeroIncremento += 1;
+// numeroIncremento = numeroIncremento + 1;
+numeroIncremento++;
+Console.WriteLine(numeroIncremento);
+
+
+
+int numeroDecremento = 20;
+
+Console.WriteLine(numeroDecremento);
+Console.WriteLine("Decrementando o 20");
+numeroDecremento--;
+Console.WriteLine(numeroDecremento);
