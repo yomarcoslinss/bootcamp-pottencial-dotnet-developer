5f79351669b5fe1af9a9033773410a0eb293cfe4
Marcos Lins <vinicius.gzm0@gmail.com>
2022-12-06 21:00:00 -0300

    Desafio de projeto concluído

---
+++ Desafios de Projeto/trilha-net-explorando-desafio/Models/Reserva.cs
@@ -0,0 +1,49 @@
+namespace DesafioProjetoHospedagem.Models
+{
+    public class Reserva
+    {
+        public List<Pessoa> Hospedes { get; set; }
+        public Suite Suite { get; set; }
+        public int DiasReservados { get; set; }
+
+        public Reserva() { }
+
+        public Reserva(int diasReservados)
+        {
+            DiasReservados = diasReservados;
+        }
+
+        public void CadastrarHospedes(List<Pessoa> hospedes)
+        {
+            if (hospedes.Count <= Suite.Capacidade)
+            {
+                Hospedes = hospedes;
+            }
+            else
+            {
+                throw new Exception("Não foi possível concluir a sua reserva :( o número de hospedes excedeu a capacidade da suite.");
+            }
+        }
+
+        public void CadastrarSuite(Suite suite)
+        {
+            Suite = suite;
+        }
+
+        public int ObterQuantidadeHospedes()
+        {
+            return Hospedes.Count;
+        }
+
+        public decimal CalcularValorDiaria()
+        {
+            decimal valor = DiasReservados * Suite.ValorDiaria;
+
+            if (DiasReservados >= 10)
+            {
+                valor = (valor / 100) * 90;
+            }
+            return valor;
+        }
+    }
+}
