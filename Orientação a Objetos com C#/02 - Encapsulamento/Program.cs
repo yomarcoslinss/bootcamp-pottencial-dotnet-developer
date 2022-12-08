9a45b12ce8247b99c3b66bc35aa76875533b7378
Marcos Lins <vinicius.gzm0@gmail.com>
2022-12-08 04:37:41 -0300

    Curso "Introdução, Abstração e Encapsulamento com C# " completo

--- Orientação a Objetos com C#/02 - Encapsulamento/Program.cs
+++ Orientação a Objetos com C#/02 - Encapsulamento/Program.cs
@@ -1,2 +1,12 @@
 ﻿using _02___Encapsulamento.Models;
 
+
+ContaCorrente c1 = new ContaCorrente(3597, 200);
+
+//c1.saldo = 5000; // Irá me retornar um erro pois esse campo é inacessível devido a sua proteção
+
+c1.ExibirSaldo();
+
+c1.Sacar(150);
+
+c1.ExibirSaldo();
