87c40ef8ff3baf4bb39e62ccd68a17517bf77e55
Marcos Lins <vinicius.gzm0@gmail.com>
2023-01-02 20:51:04 -0300

    Módulo "Frontend com ASP.NET MVC" completo

--- Dominando o Ecossistema .NET/02 - Projeto MVC/Controllers/ContatoController.cs
+++ Dominando o Ecossistema .NET/02 - Projeto MVC/Controllers/ContatoController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using ProjetoMVC.Context;
+using ProjetoMVC.Models;
 
 namespace ProjetoMVC.Controllers
 {
@@ -21,5 +22,86 @@ namespace ProjetoMVC.Controllers
             var contatos = _context.Contatos.ToList();
             return View(contatos);
         }
+
+        public IActionResult Criar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Criar(Contato contato)
+        {
+            if(ModelState.IsValid)
+            {
+                _context.Contatos.Add(contato);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(contato);
+        }
+
+        [HttpGet]
+        public IActionResult Editar(int id)
+        {
+            var contato = _context.Contatos.Find(id);
+
+            if(contato == null)
+            {
+                
+                return RedirectToAction(nameof(Index));
+            }
+            return View(contato);
+        }
+
+        [HttpPost]
+        public IActionResult Editar(Contato contato)
+        {
+            var contatoBanco = _context.Contatos.Find(contato.Id);
+
+            contatoBanco.Nome = contato.Nome;
+            contatoBanco.Telefone = contato.Telefone;
+            contatoBanco.Ativo = contato.Ativo;
+
+            _context.Contatos.Update(contatoBanco);
+            _context.SaveChanges();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet]
+        public IActionResult Detalhes(int id)
+        {
+            var contatoBanco = _context.Contatos.Find(id);
+            if(contatoBanco == null)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(contatoBanco);
+        }
+
+        [HttpGet]
+        public IActionResult Deletar(int id)
+        {
+            var contatoBanco = _context.Contatos.Find(id);
+            if(contatoBanco == null)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(contatoBanco);
+        }
+
+        [HttpPost]
+        public IActionResult Deletar(Contato contato)
+        {
+            var contatoBanco = _context.Contatos.Find(contato.Id);
+
+            _context.Contatos.Remove(contatoBanco);
+            _context.SaveChanges();
+
+            return RedirectToAction(nameof(Index));
+        }
+ 
     }
 }
